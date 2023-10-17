b9a6c44883c651557ae7f9ba9f6ada9085b6b386
npokrandt <npokrandt919@gmail.com>
2023-10-17 01:16:24 -0500

    adds the indexeddb put and get functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,32 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('textditor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('textditor')) {
+        console.log('textditor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('textditor', { keyPath: 'id', autoIncrement: true });
+      console.log('textditor database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('textditor', 1)
+  content = {content, id: 1}
+  const transaction = db.transaction('textditor', 'readwrite')
+  const store = transaction.store
+  return await store.put(content)
+  
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('textditor', 1)
+  const transaction = db.transaction('textditor', 'readonly')
+  const store = transaction.store
+  const everything = await store.get(1)
+  return everything.content
+}
 
 initdb();
